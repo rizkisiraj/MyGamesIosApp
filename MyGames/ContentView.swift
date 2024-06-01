9217fd7b30f38fa7816f3d2f800fc581c3f5d21b
rizkisiraj <jomirwa@gmail.com>
2024-06-01 22:21:44 +0700

    Initial Commit

---
+++ MyGames/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyGames
+//
+//  Created by Rizki Siraj on 01/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyGames/MyGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGamesApp.swift
+//  MyGames
+//
+//  Created by Rizki Siraj on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MyGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
