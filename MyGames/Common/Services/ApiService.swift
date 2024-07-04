cc91eb76721667530d9e7fd1fccca734f668701d
rizkisiraj <jomirwa@gmail.com>
2024-07-04 15:36:14 +0700

    feat: add network manager

---
+++ MyGames/Common/Services/ApiService.swift
@@ -0,0 +1,8 @@
+//
+//  ApiService.swift
+//  MyGames
+//
+//  Created by Rizki Siraj on 04/07/24.
+//
+
+import Foundation
---
+++ MyGames/Models/Game.swift
@@ -0,0 +1,8 @@
+//
+//  Game.swift
+//  MyGames
+//
+//  Created by Rizki Siraj on 04/07/24.
+//
+
+import Foundation
