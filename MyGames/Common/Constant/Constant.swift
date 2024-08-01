316048546e4901364129c62bc0db28c2cd0cb28e
rizkisiraj <jomirwa@gmail.com>
2024-08-01 23:00:49 +0700

    feat: add env

---
+++ MyGames/Common/Constant/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  MyGames
+//
+//  Created by Rizki Siraj on 01/08/24.
+//
+
+import Foundation
---
+++ MyGames/ViewModels/GameVM.swift
@@ -0,0 +1,9 @@
+//
+//  GameViewModel.swift
+//  MyGames
+//
+//  Created by Rizki Siraj on 01/08/24.
+//
+
+import Foundation
+
